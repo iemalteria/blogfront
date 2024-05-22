ae50f744d38dd4853713aac456863404cf802af1
santiagojimenezv <s.jimenezv10@gmail.com>
2024-05-22 10:08:22 -0500

    Frontend Uploaded

---
+++ src/pages/BlogPage.js
@@ -0,0 +1,37 @@
+import Post from '../Post';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function BlogPage(){
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const axiosInstance = axios.create({
+            withCredentials: true, // This is crucial to include cookies in your requests
+        });
+
+        axiosInstance.get('https://iemalteria-back.vercel.app/post')
+            .then(response => {
+                setPosts(response.data);
+                console.log(response);
+            })
+            .catch(error => {
+                console.error('Error fetching posts:', error);
+            });
+
+        return () => {
+            axiosInstance.interceptors.request.eject(axiosInstance.interceptors.request.handlers.length - 1);
+        };
+    }, []);
+
+    return (
+        <div className='post-list-container'>
+            <div className='post-list'>
+                <h1 className='post-title'>PUBLICACIONES</h1>
+                {posts.length > 0 && posts.map(post => (
+                    <Post key={post._id} {...post} />
+                ))}
+            </div>
+        </div>
+    );
+}
